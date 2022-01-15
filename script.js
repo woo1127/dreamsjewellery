2b90e7f2a3fcb9f78b9dc253846ee14ec2b5bbf8
Jeremy Woo <71725118+woo1127@users.noreply.github.com>
2022-01-15 16:46:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navBar = document.getElementsByClassName('navbar')[0]
-const navItem0 = document.getElementsByClassName('nav-item')[0]
-const navItem1 = document.getElementsByClassName('nav-item')[1]
-const navItem2 = document.getElementsByClassName('nav-item')[2]
-const navItem3 = document.getElementsByClassName('nav-item')[3]
-const navItem4 = document.getElementsByClassName('nav-item')[4]
-const navItem5 = document.getElementsByClassName('nav-item')[5]
+
+const navItems = document.getElementsByClassName('nav-item')
 
 toggleButton.addEventListener('click', () => {
     navBar.classList.toggle('active')
     navBar.classList.toggle('passive', false)
 })
 
-
-navItem0.addEventListener('click', () => {
-    navBar.classList.toggle('active', false)
-    navBar.classList.toggle('passive')
-})
-
-navItem1.addEventListener('click', () => {
-    navBar.classList.toggle('active', false)
-    navBar.classList.toggle('passive')
-})
-
-navItem2.addEventListener('click', () => {
-    navBar.classList.toggle('active', false)
-    navBar.classList.toggle('passive')
-})
-
-navItem3.addEventListener('click', () => {
-    navBar.classList.toggle('active', false)
-    navBar.classList.toggle('passive')
-})
-
-navItem4.addEventListener('click', () => {
-    navBar.classList.toggle('active', false)
-    navBar.classList.toggle('passive')
-})
+for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', () => {
+        navBar.classList.toggle('active', false)
+        navBar.classList.toggle('passive')
+    })
+}
 
 const buttons = document.querySelectorAll('[data-carousel-button]')
 
